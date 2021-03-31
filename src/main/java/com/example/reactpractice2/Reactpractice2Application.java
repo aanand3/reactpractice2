2f713e088cc00b90348b55dc944ee6054d61247a
aanand3 <as5265@columbia.edu>
2021-03-31 09:55:11 -0500

    first commit

---
+++ src/main/java/com/example/reactpractice2/Reactpractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.reactpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactpractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reactpractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactpractice2/Reactpractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactpractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
